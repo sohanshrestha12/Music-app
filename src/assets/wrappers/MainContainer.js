78651840a3f5f75fdc272c97f0474cb37f895bc9
sohanshrestha12 <sohanshrestha40@gmail.com>
2023-12-31 22:02:03 +0545

    cards

--- src/assets/wrappers/MainContainer.js
+++ src/assets/wrappers/MainContainer.js
@@ -4,7 +4,7 @@ const Wrapper = styled.section`
   width: 75vw;
   background-color: #121212;
   border-radius: 7px;
-  margin:15px 10px 0 5px;
+  margin: 15px 10px 0 5px;
 
   .header {
     display: flex;
@@ -14,6 +14,66 @@ const Wrapper = styled.section`
   }
   .header > * {
     padding: 20px;
+  }
+  .spotifyPlaylists {
+    padding: 16px;
+  }
+  .spotifyPlaylists h1 {
+    padding: 0 0 0 20px;
+  }
+  .spotifyPlaylists .cardContainer {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+    gap: 30px;
+    padding: 20px;
+  }
+  .card > * {
+    padding-top: 3px;
+  }
+  .card h2 {
+    font-size: 18px;
+  }
+  .card p {
+    font-size: 12px;
+    color: #a7a7a7;
+  }
+  .card .cardImage {
+    position: relative;
+  }
+  .card .play {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    background-color: #1edc62;
+    position: absolute;
+    opacity: 0;
+    transition:all 0.3s ease;
+    bottom: -10px;
+    right: 10px;
+  }
+  .card:hover{
+    cursor: pointer;
+  }
+  .card:hover .play{
+    opacity:1;
+    bottom:10px;
+  }
+  .card .play svg {
+    font-size: 21px;
+    color: black;
+    padding-left:2px;
+  }
+  .spotifyPlaylists .card {
+    padding: 10px 10px 15px 10px;
+    border-radius: 5px;
+    background-color: #252525;
+  }
+  .spotifyPlaylists .cardContainer .card .cardImage img {
+    width: 100%;
+    object-fit: contain;
   }
 `;
 
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import Wrapper from '../assets/wrappers/MainContainer'
+import React from "react";
+import Wrapper from "../assets/wrappers/MainContainer";
 import { FaChevronRight } from "react-icons/fa";
 import { FaChevronLeft } from "react-icons/fa";
+import { FaPlay } from "react-icons/fa";
 
 const MainContainer = () => {
   return (
@@ -16,11 +17,26 @@ const MainContainer = () => {
           <button>Login</button>
         </div>
       </div>
-      <div className='spotify-playlists'>
+      <div className="spotifyPlaylists">
         <h1>Spotify Playlists</h1>
+        <div className="cardContainer">
+          <div className="card">
+            <div className="cardImage">
+              <img
+                src="https://i.scdn.co/image/ab67706f000000026e515187c071e45918e9f0de"
+                alt="playlist-img"
+              />
+              <div className="play">
+                <FaPlay />
+              </div>
+            </div>
+            <h2>Dinner with Friends</h2>
+            <p>The perfect soundtrack to those long nights over dinner</p>
+          </div>
+        </div>
       </div>
     </Wrapper>
   );
-}
+};
 
-export default MainContainer
+export default MainContainer;
